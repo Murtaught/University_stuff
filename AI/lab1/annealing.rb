7199847e5a1ccf55e57c82f8f7179bb07a4f0883
Ivan Mikhnovich <murtaught@gmail.com>
2014-12-02 08:35:40 +0300

    Added AI/Lab1

---
+++ AI/lab1/annealing.rb
@@ -0,0 +1,95 @@
+﻿# Давайте реализуем метод имитации отжига на Руби
+module AI_Lab1
+	ANNEALING_MAX_ITERATIONS = 10000
+	ANNEALING_TEMP_RADIUS = 0.7
+
+	def self.rand_float_in(range)
+		return rand * (range.max - range.min) + range.min
+	end
+	
+	def self.get_random_point_in(search_area)
+		point = []
+		search_area.each { |range| point.push(rand_float_in range) }
+		return point
+	end
+	
+	def self.dist(a, b)
+		sum_sq = 0
+		a.each_index { |i| sum_sq += (a[i] - b[i]) ** 2 }
+		return Math.sqrt(sum_sq)
+	end
+	
+	def self.get_candidate(cur_point, temp, search_area)
+		radius = temp * ANNEALING_TEMP_RADIUS
+		
+		area = search_area.dup
+		area.each_index do |i|
+			area[i] = ((cur_point[i] - radius / 2)..(cur_point[i] + radius / 2))
+		end
+		
+		begin
+			point = get_random_point_in area
+		end until dist(point, cur_point) < radius
+		return point
+	end
+	
+	def self.transition_probability(delta_energy, temp)
+		return Math.exp( -delta_energy.to_f / temp )
+	end
+	
+	def self.get_temp(start_temp, iteration_number)
+		return start_temp.to_f / iteration_number
+		#return start_temp.to_f / Math.log(iteration_number + 1) 
+	end
+	
+	# Метод изначально создавался способным обрабатывать функции с произвольным числом параметров 
+	def self.annealing_simulation(f, search_area, start_temp, min_temp, start_point = nil)
+		# 1. выбираем начальную точку
+		if start_point == nil 
+			start_point = get_random_point_in search_area
+		end
+		
+		graph_info = [] # вектор из чисел: значение минимума на i-той итерации
+		
+		# Считаем значение функции в начальной точке
+		cur_state  = start_point
+		cur_energy = f[*cur_state]
+		cur_temp = start_temp
+		
+		graph_info << cur_energy
+		
+		(1..ANNEALING_MAX_ITERATIONS).each do |iter|
+			# В цикле
+			# В качестве кандидата на роль следующей точки возьмем случайную точку
+			# интересно что даже в таком примитивном варианте метод отжига хорошо работает
+			candidate_state  = get_candidate(cur_state, cur_temp, search_area)
+			candidate_energy = f[*candidate_state]
+			
+			if (candidate_energy < cur_energy) || (rand < transition_probability( candidate_energy - cur_energy, cur_temp ))
+				cur_state  = candidate_state
+				cur_energy = candidate_energy
+			end
+			
+			graph_info << cur_energy
+			
+			# Уменьшаем температуру
+			cur_temp = get_temp(start_temp, iter)
+			
+			break if cur_temp <= min_temp
+		end
+		
+		return { :answer        => cur_state, 
+				 :minimum_value => f[*cur_state], 
+				 :iterations    => graph_info.size, 
+				 :graph_info    => graph_info }
+	end
+end
+
+f = -> x, y { (x - 5)**2 + (y + 1)**2 - 7 }
+
+area = [ (-10.0..10.0), (-10.0..10.0) ]
+
+results = AI_Lab1.annealing_simulation(f, area, 50, 0.01)
+p results[:answer]
+p results[:minimum_value]
+p results[:iterations]
---
+++ AI/lab1/gradient_descend.rb
@@ -0,0 +1,85 @@
+﻿module AI_Lab1
+	GRADIENT_DELTA    = 0.000001
+	GRAD_DESC_STEP    = 0.2
+	GRAD_DESC_EPSILON = 0.000001
+	
+	def self.rand_float_in(range)
+		return rand * (range.max - range.min) + range.min
+	end
+	
+	def self.gradient(f, x) # Численно вычисляет градиент ф-ии f в (.)x
+		g = []
+		x_copy = x.dup
+		
+		x_copy.each_index do |i| # для каждой координаты точки...
+			x_copy[i] += GRADIENT_DELTA
+			g.push( (f[*x_copy] - f[*x]) / GRADIENT_DELTA ) 
+			x_copy[i] -= GRADIENT_DELTA
+		end
+		
+		return g
+	end
+	
+	def self.normalize(vector)
+		vector.map! { |coord| coord.zero? ? 0.0 : coord.to_f / coord.abs }
+	end
+	
+	# Метод изначально создавался способным обрабатывать функции с произвольным числом параметров 
+	def self.gradient_descend(f, search_area, start_point = nil)
+		# 1. выбираем начальную точку
+		if start_point == nil 
+			start_point = []
+			search_area.each { |range| start_point.push(rand_float_in range) }
+		end
+		
+		graph_info = [] # вектор из чисел: значение минимума на i-той итерации
+		iteration_number = 0
+		
+		# Считаем значение функции в начальной точке
+		old_value = f[*start_point]
+		cur_point = start_point
+		
+		graph_info << old_value
+		
+		loop do
+			iteration_number += 1
+		
+			# В цикле
+			# Считаем градиент функции в текущей точке
+			g = gradient(f, cur_point)
+			
+			# Нормализуем градиент (необходимость? спорный вопрос)
+			#normalize(g)
+			
+			# Сдвигаемся по градиенту
+			cur_point.size.times do |i|
+				cur_point[i] -= g[i] * GRAD_DESC_STEP
+			end
+			
+			# Считаем функцию в новой точке
+			new_value = f[*cur_point]
+			graph_info << new_value
+			
+			# Eсли изменение значения ничтожно мало - эта новая точка становится ответом
+			break if (new_value - old_value).abs < GRAD_DESC_EPSILON
+			
+			# Иначе помечаем это значение как предыдущее и 
+			# выполняем еще одну итерацию
+			old_value = new_value
+		end
+		
+		return { :answer        => cur_point, 
+				 :minimum_value => f[*cur_point], 
+				 :iterations    => iteration_number, 
+				 :graph_info    => graph_info }
+	end
+end
+
+f = -> x, y { (x - 5)**2 + (y + 1)**2 - 7 }
+
+area = [ (-10.0..10.0), (-10.0..10.0) ]
+
+results = AI_Lab1.gradient_descend(f, area)
+p results[:answer]
+p results[:minimum_value]
+p results[:iterations]
